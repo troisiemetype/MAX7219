d9e41e593873b8a2c4e7eaccbaacff43612556a9
Pierre-Loup Martin <pierreloupm@gmail.com>
2024-01-10 20:18:05 +0100

    Modified example.

--- examples/bargraph/bargraph.ino
+++ examples/bargraph/bargraph.ino
@@ -6,6 +6,8 @@ const uint8_t PIN_LOAD = 3;
 
 uint32_t counter = 1;
 
+uint8_t bar = 0;
+
 MAX7219 max;
 
 void setup(){
@@ -18,13 +20,25 @@ void setup(){
 }
 
 void loop(){
-	max.setDigit(7, counter);
+//	max.setDigit(7, counter);
 
 //	counter++;
-	counter *= 2;
+//	counter *= 2;
 //	counter |= 0xFF;
 
-	if(counter >= 127) counter = 1;
+//	if(counter >= 127) counter = 1;
+
+	if(bar >= 30) bar = 0;
+
+	uint8_t bit = bar % 8;
+	uint8_t segment = bar / 8;
+
+	for(uint8_t i = 0; i < 4; i++){
+		if(i == segment) max.setDigit(7 - i, 1 << bit);
+		else max.setDigit(7 - i, 0);
+	}
+
+	bar++;
 
 	delay(250);
 }
